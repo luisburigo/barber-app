79445bf516d3e0d2178c84032282ce01ba4ff34a
Luís Burigo <guga.burigo29@outlook.com>
2020-08-11 21:42:01 -0300

    add: implements typescript

---
+++ client-ts/src/layouts/dashboard/components/MenuList/menuItens.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBuilding, faIdCard, faCogs, faClock, faUsers, faCalendarDay, faHome} from "@fortawesome/free-solid-svg-icons";
+
+const createItem = (icon, link, name) => ({icon, link, name});
+
+const menuItens = [
+    createItem(<FontAwesomeIcon icon={faHome}/>, "/dashboard", "Dashboard"),
+    createItem(<FontAwesomeIcon icon={faIdCard}/>, "/dashboard/funcionarios", "Funcionarios"),
+    createItem(<FontAwesomeIcon icon={faCogs}/>, "/dashboard/servicos", "Serviços"),
+    createItem(<FontAwesomeIcon icon={faCalendarDay}/>, "/dashboard/horarios", "Horarios"),
+    createItem(<FontAwesomeIcon icon={faBuilding}/>, "/dashboard/unidades", "Unidades"),
+    createItem(<FontAwesomeIcon icon={faUsers}/>, "/dashboard/clientes", "Clientes"),
+    createItem(<FontAwesomeIcon icon={faClock}/>, "/dashboard/jornadas", "Jornadas de trabalho"),
+];
+
+export default menuItens;
